fb097ba46f796c2f0a41b2e931b875135a552595
Cyril ravot <cyril.ravot@hotmail.fr>
2022-08-12 23:43:36 +0200

    Modif cors Test encore en cours ..

--- backend/app.js
+++ backend/app.js
@@ -29,8 +29,9 @@ const corsOption = {
 }
 app.use(cors(corsOption));
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://rossignol-peinture.fr');
-    res.setHeader('Access-Control-Allow-Origin', 'https://api.rossignol-peinture.fr');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    // res.setHeader('Access-Control-Allow-Origin', 'https://rossignol-peinture.fr');
+    // res.setHeader('Access-Control-Allow-Origin', 'https://api.rossignol-peinture.fr');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
